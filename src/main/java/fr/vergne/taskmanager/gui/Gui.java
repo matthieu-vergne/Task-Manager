6eba50089512a2500f079f78e31f2e2e5c17599f
vergne <vergne@fbk.eu>
2012-02-25 18:29:42 +0100

    Remove auto-generated TODOs.

--- src/main/java/fr/vergne/taskmanager/gui/Gui.java
+++ src/main/java/fr/vergne/taskmanager/gui/Gui.java
@@ -76,48 +76,42 @@ public class Gui extends JFrame {
 				component.transferFocus();
 			}
 		});
-		
+
 		addWindowListener(new WindowListener() {
-			
+
 			@Override
 			public void windowOpened(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				// nothing to do
 			}
-			
+
 			@Override
 			public void windowIconified(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				// nothing to do
 			}
-			
+
 			@Override
 			public void windowDeiconified(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				// nothing to do
 			}
-			
+
 			@Override
 			public void windowDeactivated(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				// nothing to do
 			}
-			
+
 			@Override
 			public void windowClosing(WindowEvent arg0) {
 				saveTasks(saveFile);
 			}
-			
+
 			@Override
 			public void windowClosed(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				// nothing to do
 			}
-			
+
 			@Override
 			public void windowActivated(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				// nothing to do
 			}
 		});
 	}
