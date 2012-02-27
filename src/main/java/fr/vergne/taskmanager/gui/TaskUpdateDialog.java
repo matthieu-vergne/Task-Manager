eead1545772fdc573f5561ec337abb15d6f66bed
vergne <vergne@fbk.eu>
2012-02-27 22:54:19 +0100

    Manage manually modified dates.

--- src/main/java/fr/vergne/taskmanager/gui/TaskUpdateDialog.java
+++ src/main/java/fr/vergne/taskmanager/gui/TaskUpdateDialog.java
@@ -4,11 +4,15 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -192,84 +196,33 @@ public class TaskUpdateDialog extends JDialog {
 
 			});
 
-			KeyListener keyListener = new KeyListener() {
+			dateField.addFocusListener(new FocusListener() {
+				
+				@Override
+				public void focusLost(FocusEvent arg0) {
+					applyValue(dateField.getText());
+				}
+				
+				@Override
+				public void focusGained(FocusEvent arg0) {
+					// do nothing
+				}
+			});
+			dateField.addKeyListener(new KeyListener() {
 
 				@Override
 				public void keyTyped(KeyEvent arg0) {
 					// do nothing
 				}
 
+				private final Collection<Integer> stepKeys = Arrays.asList(
+						KeyEvent.VK_DOWN, KeyEvent.VK_UP, KeyEvent.VK_PAGE_UP,
+						KeyEvent.VK_PAGE_DOWN);
+
 				@Override
 				public void keyReleased(KeyEvent event) {
-					int caretPosition = dateField.getCaretPosition();
-
-					int unit;
-					if (caretPosition > 16) {
-						unit = Calendar.SECOND;
-					} else if (caretPosition > 13) {
-						unit = Calendar.MINUTE;
-					} else if (caretPosition > 10) {
-						unit = Calendar.HOUR_OF_DAY;
-					} else if (caretPosition > 7) {
-						unit = Calendar.DAY_OF_MONTH;
-					} else if (caretPosition > 4) {
-						unit = Calendar.MONTH;
-					} else if (caretPosition >= 0) {
-						unit = Calendar.YEAR;
-					} else {
-						throw new RuntimeException(
-								"This case should not happen.");
-					}
-
-					int step;
-					if (event.getKeyCode() == KeyEvent.VK_DOWN) {
-						step = -1;
-					} else if (event.getKeyCode() == KeyEvent.VK_UP) {
-						step = 1;
-					} else if (event.getKeyCode() == KeyEvent.VK_PAGE_UP) {
-						switch (unit) {
-						case Calendar.SECOND:
-						case Calendar.MINUTE:
-						case Calendar.DAY_OF_MONTH:
-						case Calendar.YEAR:
-							step = 10;
-							break;
-						case Calendar.HOUR_OF_DAY:
-						case Calendar.MONTH:
-							step = 6;
-							break;
-
-						default:
-							throw new RuntimeException(unit
-									+ " is not a managed case.");
-						}
-					} else if (event.getKeyCode() == KeyEvent.VK_PAGE_DOWN) {
-						switch (unit) {
-						case Calendar.SECOND:
-						case Calendar.MINUTE:
-						case Calendar.DAY_OF_MONTH:
-						case Calendar.YEAR:
-							step = -10;
-							break;
-						case Calendar.HOUR_OF_DAY:
-						case Calendar.MONTH:
-							step = -6;
-							break;
-
-						default:
-							throw new RuntimeException(unit
-									+ " is not a managed case.");
-						}
-					} else {
-						step = 0;
-					}
-
-					if (step != 0) {
-						calendar.add(unit, step);
-						updateDateField();
-						dateField.setCaretPosition(caretPosition);
-					} else {
-						// update nothing
+					if (stepKeys.contains(event.getKeyCode())) {
+						applyStep(event.getKeyCode());
 					}
 				}
 
@@ -277,9 +230,7 @@ public class TaskUpdateDialog extends JDialog {
 				public void keyPressed(KeyEvent arg0) {
 					// do nothing
 				}
-			};
-
-			dateField.addKeyListener(keyListener);
+			});
 		}
 
 		private void initComponents() {
@@ -325,6 +276,130 @@ public class TaskUpdateDialog extends JDialog {
 		@Override
 		public synchronized KeyListener[] getKeyListeners() {
 			return dateField.getKeyListeners();
+		}
+
+		private void applyValue(String text) {
+			calendar.setTime(new Date(0));
+			if (text.matches("\\d{4}\\D?.*")) {
+				int year = Integer.parseInt(text.substring(0, 4));
+				calendar.set(Calendar.YEAR, year);
+
+				if (text.matches("\\d{4}-\\d{2}\\D?.*")) {
+					int month = Integer.parseInt(text.substring(5, 7));
+					calendar.set(Calendar.MONTH, month - 1);
+				} else {
+					calendar.set(Calendar.MONTH,
+							calendar.getMinimum(Calendar.MONTH));
+				}
+
+				if (text.matches("\\d{4}-\\d{2}-\\d{2}\\D?.*")) {
+					int day = Integer.parseInt(text.substring(8, 10));
+					calendar.set(Calendar.DAY_OF_MONTH, day);
+				} else {
+					calendar.set(Calendar.DAY_OF_MONTH,
+							calendar.getMinimum(Calendar.DAY_OF_MONTH));
+				}
+
+				if (text.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}\\D?.*")) {
+					int hour = Integer.parseInt(text.substring(11, 13));
+					calendar.set(Calendar.HOUR_OF_DAY, hour);
+				} else {
+					calendar.set(Calendar.HOUR_OF_DAY,
+							calendar.getMinimum(Calendar.HOUR_OF_DAY));
+				}
+
+				if (text.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}\\D?.*")) {
+					int minute = Integer.parseInt(text.substring(14, 16));
+					calendar.set(Calendar.MINUTE, minute);
+				} else {
+					calendar.set(Calendar.MINUTE,
+							calendar.getMinimum(Calendar.MINUTE));
+				}
+
+				if (text.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\D?.*")) {
+					int second = Integer.parseInt(text.substring(17, 19));
+					calendar.set(Calendar.SECOND, second);
+				} else {
+					calendar.set(Calendar.SECOND,
+							calendar.getMinimum(Calendar.SECOND));
+				}
+
+				calendar.set(Calendar.MILLISECOND,
+						calendar.getMinimum(Calendar.MILLISECOND));
+			} else {
+				calendar.setTime(new Date());
+			}
+			updateDateField();
+		}
+
+		private void applyStep(int keyCode) {
+			int caretPosition = dateField.getCaretPosition();
+
+			int unit;
+			if (caretPosition > 16) {
+				unit = Calendar.SECOND;
+			} else if (caretPosition > 13) {
+				unit = Calendar.MINUTE;
+			} else if (caretPosition > 10) {
+				unit = Calendar.HOUR_OF_DAY;
+			} else if (caretPosition > 7) {
+				unit = Calendar.DAY_OF_MONTH;
+			} else if (caretPosition > 4) {
+				unit = Calendar.MONTH;
+			} else if (caretPosition >= 0) {
+				unit = Calendar.YEAR;
+			} else {
+				throw new RuntimeException("This case should not happen.");
+			}
+
+			int step;
+			if (keyCode == KeyEvent.VK_DOWN) {
+				step = -1;
+			} else if (keyCode == KeyEvent.VK_UP) {
+				step = 1;
+			} else if (keyCode == KeyEvent.VK_PAGE_UP) {
+				switch (unit) {
+				case Calendar.SECOND:
+				case Calendar.MINUTE:
+				case Calendar.DAY_OF_MONTH:
+				case Calendar.YEAR:
+					step = 10;
+					break;
+				case Calendar.HOUR_OF_DAY:
+				case Calendar.MONTH:
+					step = 6;
+					break;
+
+				default:
+					throw new RuntimeException(unit + " is not a managed case.");
+				}
+			} else if (keyCode == KeyEvent.VK_PAGE_DOWN) {
+				switch (unit) {
+				case Calendar.SECOND:
+				case Calendar.MINUTE:
+				case Calendar.DAY_OF_MONTH:
+				case Calendar.YEAR:
+					step = -10;
+					break;
+				case Calendar.HOUR_OF_DAY:
+				case Calendar.MONTH:
+					step = -6;
+					break;
+
+				default:
+					throw new RuntimeException(unit + " is not a managed case.");
+				}
+			} else {
+				step = 0;
+			}
+
+			if (step != 0) {
+				calendar.add(unit, step);
+				updateDateField();
+				dateField.setCaretPosition(caretPosition);
+			} else {
+				// update nothing
+			}
 		}
 	}
 
